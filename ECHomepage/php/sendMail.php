98087b2eeab05527e1a0cd91eff55a16fdaad4a7
kellergoech <kellergoech@gmx.de>
2020-09-22 18:31:07 +0200

    Implementation of Email Sending via the COntact Button

---
+++ ECHomepage/php/sendMail.php
@@ -0,0 +1,63 @@
+<?php
+
+function sanitize_my_email($field) {
+    $field = filter_var($field, FILTER_SANITIZE_EMAIL);
+    if (filter_var($field, FILTER_VALIDATE_EMAIL)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function IsInjected($str)
+{
+    $injections = array('(\n+)',
+           '(\r+)',
+           '(\t+)',
+           '(%0A+)',
+           '(%0D+)',
+           '(%08+)',
+           '(%09+)'
+           );
+
+    $inject = join('|', $injections);
+    $inject = "/$inject/i";
+
+    if(preg_match($inject,$str))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+}
+
+$to      = 'leitung@ec-hormersdorf.de';
+$subject = $_POST['subject'];
+$message = $_POST['fname'] . " " . $_POST['lname'] . " wrote the following:" . "\r\n" . $_POST['message'];
+$message = filter_var($message, FILTER_SANITIZE_STRING);
+$message = wordwrap($message, 70, "\r\n");
+if (!IsInjected($_POST['email'])){
+    $headers = "Content-type: text/plain; charset=utf-8" . "\r\n" .
+            $_POST['email'] . "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
+}
+
+$result = mail($to, $subject, $message, $headers);
+//ob_start();
+if ($result){
+    echo '<script type="text/javascript">';
+    echo ' alert("Message succesfully send")';  //not showing an alert box.
+    echo '</script>';
+} else {
+    echo '<script type="text/javascript">';
+    echo ' alert("Error sending message")';  //not showing an alert box.
+    echo '</script>';
+
+}
+
+echo("<script>window.location = '../index.html';</script>");
+//header("Location: index.html");
+//ob_end_flush();
+?>

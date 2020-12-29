8b305f4973fe891fa07758cfa95b3e631b6c4bd8
GerKev-SI <66366928+GerKev-SI@users.noreply.github.com>
2020-12-29 11:33:45 +0100

    update master page (#15)
    * Jahreslosung 20/21
    
    * add index.html to root directory
    
    * Create CNAME
    
    * Delete CNAME
    
    * update gh-pages from master (#5)
    
    * Implementation of Email Sending via the COntact Button
    
    * optimization
    
    * email sanitizazion added
    
    * upd3
    
    * Create README.md
    
    * Update README.md
    
    Co-authored-by: kellergoech <kellergoech@gmx.de>
    Co-authored-by: kellergoech <38539019+kellergoech@users.noreply.github.com>
    
    * Favicon, LogoImages, Text...
    
    * Elias (#7)
    
    * Implementation of Email Sending via the COntact Button
    
    * optimization
    
    * email sanitizazion added
    
    * upd3
    
    * sanitize Email corrected
    
    * visible phone calender set - iframe has to be adjusted
    
    * calender test
    
    Co-authored-by: GerKev-SI <66366928+GerKev-SI@users.noreply.github.com>
    
    * add links, center calendar, comment uncomplete items
    
    * mobile calendar (#8)
    
    * edit facebook instagramm link
    
    * mail an PHP 4.4 angepasst
    
    * Merge automatic Action to dev Branch (#11)
    
    * Automatic Action for deploying (#10)
    
    * Automatic Action for deploying
    
    hab die Action mal angelegt, können wir ja mal testen ob das funktioniert, da spare wir uns das FTP tool :)
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    Co-authored-by: kellergoech <38539019+kellergoech@users.noreply.github.com>
    
    * set contactform elements as required and extend email header
    
    * add homepage manifest
    
    * move manifest
    
    * add LKG Site
    
    * update lkg calendar
    
    * php email addresse in index html als variable
    
    * Update main.yml
    
    LKG -> LKG
    ECHomepage -> ECHomepage
    common -> LKG/common
    common -> ECHomepage/common
    
    * add "Chronik der LKG"
    
    Co-authored-by: kellergoech <kellergoech@gmx.de>
    Co-authored-by: kellergoech <38539019+kellergoech@users.noreply.github.com>
    Co-authored-by: GerKev-HO <71837047+GerKev-HO@users.noreply.github.com>
    Co-authored-by: Marie-Elaine Marie-elainekevingerhardt Kevin Gerhardt <marie-elainekevingerhardt@MarieElineKevin.fritz.box>

--- README.md
+++ README.md
@@ -1,21 +1,3 @@
 # ECHomepage
 EC Hormersdorf Website
-
-
-Für das Einbinden des GoogleKalender ein Iframe benutzen:
-
-https://profromgo.com/blog/google-calendar-responsive/
-
-Farben als variable:
-
-:root {
-  --main-bg-color: coral;
-}
-
-#div1 {
-  background-color: var(--main-bg-color);
-}
-
-#div2 {
-  background-color: var(--main-bg-color);
-}
+LKG Hormersdorf Website
---
+++ common/php/sendMail.php
@@ -0,0 +1,68 @@
+<?php
+$message = "0";
+$email = "0";
+$subject = "0";
+$fname = "0";
+$lname = "0";
+$from = "0";
+$to = "0";
+
+function IsInjected($str)
+{
+    $injections = array('(\n+)',
+           '(\r+)',
+           '(\t+)',
+           '(%0A+)',
+           '(%0D+)',
+           '(%08+)',
+           '(%09+)'
+           );
+
+    $inject = join('|', $injections);
+    $inject = "/$inject/i";
+
+    return (preg_match($inject,$str));
+}
+
+$to      = $_POST['targetemail']; //'leitung@ec-hormersdorf.de'
+$from    = $_POST['frommail']; //kontaktformular@ec-hormersdorf.de
+$subject = $_POST['subject'];
+//ab hier nur fuer PHP <5.2
+if (preg_match("/^[a-zA-Z-' ]*$/",$_POST['fname'])) {
+  if (preg_match("/^[a-zA-Z-' ]*$/",$_POST['lname'])) {
+    if (preg_match("/^[a-zA-Z-_\,\.\!' \?]*$/",$_POST['message'])) {
+      $message = $_POST['fname'] . " " . $_POST['lname'] . " wrote the following:" . "\r\n" . $_POST['message'];
+    }
+  }
+}
+//$message = filter_var($message, FILTER_SANITIZE_STRING);
+$message = wordwrap($message, 70, "\r\n");
+
+//ab hier nur fuer PHP <5.2
+if (preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i",$_POST['email'])) {
+  $email = $_POST['email'];
+}
+//$email  = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+if (!IsInjected($email)){
+    $headers  = "Content-type: text/plain; charset=utf-8" . "\r\n".
+                "From: "         . $from                  . "\r\n".
+                "Reply-To: "     . $email                 . "\r\n".
+                'X-Mailer: PHP/' . phpversion();
+}
+
+if (!(empty($message) or empty($email))){
+  $result = mail($to, $subject, $message, $headers);
+}
+
+if ($result){
+    echo '<script type="text/javascript">';
+    echo ' alert("Message succesfully send")';  //not showing an alert box.
+    echo '</script>';
+} else {
+    echo '<script type="text/javascript">';
+    echo ' alert("Error sending message")';  //not showing an alert box.
+    echo '</script>';
+}
+
+echo("<script>window.location = '../index.html';</script>");
+?>

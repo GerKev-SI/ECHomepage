24c7cace103821a7e4e38129fc74a3e59c2cc67d
kellergoech <38539019+kellergoech@users.noreply.github.com>
2021-01-30 10:01:14 +0100

    Update losung.js

--- common/js/losung.js
+++ common/js/losung.js
@@ -3,13 +3,15 @@ xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) // 4: request finished and response is ready;  200: Status ok
     {
         var splitresult = String(this.responseText).split("###");
-        if (splitresult.length > 4) {
+        if (splitresult.length > 6) {
             document.getElementById("LosungHeader").innerHTML = "Losung " + splitresult[0];
             document.getElementById("LosungContent").innerHTML = splitresult[1];
             document.getElementById("LosungFooter").innerHTML = "&mdash; " + splitresult[2];
+            document.getElementById("LosungFooter").href= splitresult[5];
             document.getElementById("LehrtextHeader").innerHTML = "Lehrtext " + splitresult[0];
             document.getElementById("LehrtextContent").innerHTML = splitresult[3];
             document.getElementById("LehrtextFooter").innerHTML = "&mdash; " + splitresult[4];
+            document.getElementById("LehrtextFooter").href= splitresult[6];
         }
     }
 };
